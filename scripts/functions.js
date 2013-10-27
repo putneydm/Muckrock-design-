6e404baac09ff48eae71217c7632beec19a6a948
putneydm <david@davidputney.com>
2013-10-27 13:28:51 -0400

    Trying to back out of something that I messed up.

---
+++ scripts/functions.js
@@ -0,0 +1,161 @@
+$(document) .ready (function()
+	{	
+
+	
+	function mobile_menu (document_width)
+		{
+	//	$('#menu') .width (document_width);
+	//	$('#menu') .height (800);
+		
+	if (embiggened 	=== 0)
+	{	
+		$( "#menu" ).animate({
+		width: document_width,
+		height: "800"
+		}, 1000, function() {
+		embiggened = 1;
+		});			
+  	}
+  	
+  	
+  	if (embiggened 	=== 1)
+	{	
+		$( "#menu" ).animate({
+		width: '45',
+		height: '45'
+		}, 1000, function() {
+		embiggened = 0;
+		});			
+  	}
+
+
+		}
+
+			menu_status = 0;		
+
+
+	
+	/*
+   var scroll_position = $(document) .scrollTop ();    
+	    if (scroll_position == 0)
+	    {
+			$('#menu').hide (function ()
+			{
+			});
+		}	
+
+	*/
+		
+	embiggened = 0;	
+	$('#menu_trigger') .click (function()
+		{
+		var document_width = .94 * $(window) .width ();
+		console.log (document_width);
+		mobile_menu(document_width);
+		});	
+		
+		
+	function show_hide_toolbox()
+		{
+
+		var hidden = $('#toolbox').height();
+		
+		console.log(hidden);
+		
+		if (hidden !== 0)
+			{
+			console.log('shows');					
+			$('#toolbox_tab') .addClass ('toolbox_minimized_tab');
+			$('#toolbox') .addClass ('toolbox_minimized');
+			}
+			
+		else
+			{
+			console.log('hidden');
+			$('#toolbox_tab') .removeClass ('toolbox_minimized_tab');
+			$('#toolbox') .removeClass ('toolbox_minimized');
+			}
+
+		}
+			
+		$('#toolbox_tab') .click (function()
+			{
+			show_hide_toolbox()
+			console.log('works');
+			});
+		
+		
+		$('#grid') .click (function()
+		{
+		$('#document_list') .fadeOut (0);
+		$('#document_list') .removeClass ('fork_document_list_view') .addClass ('fork_document_grid');	
+		$('#document_list') .fadeIn (100);	
+		});
+		
+	
+		$('#list') .click (function()
+		{
+		$('#document_list') .fadeOut (0);
+		$('#document_list') .removeClass ('fork_document_grid') .addClass ('fork_document_list_view');;	
+		$('#document_list') .fadeIn (100);	
+		});
+	
+		
+	});	
+	
+
+function menu(scroll_position, menu_height)
+		{		
+		if (scroll_position	> 100 && menu_status==0)
+		{
+		//	 $('#menu').fadeIn(100)
+		//	menu_status = 1;
+		//	console.log(menu_status);
+		
+		
+		$( "#menu" ).animate({
+		top: '0'
+		}, 200, function() {
+		menu_status = 1;
+		});		
+		
+		}
+		
+		if (scroll_position<100 && menu_status==1)
+		{
+		
+
+		$( "#menu" ).animate({
+		top: '-28'
+		}, 200, function() {
+		menu_status = 0;
+		});					
+
+			
+		}
+
+		}	
+	
+/*	
+$(window).scroll(function() {
+    clearTimeout($.data(this, 'scrollTimer'));
+    $.data(this, 'scrollTimer', setTimeout(function() {    
+    	var scroll_position = $(document) .scrollTop ();
+    	var menu_height = $('#menu') .height();
+		console.log(scroll_position);
+        menu(scroll_position, menu_height);        
+ }, 100));
+});	
+	
+*/	
+
+$(window) .resize (function()	
+	{
+	width = $('#measure_width') .width ();
+	$('#measure_width') .text (width);
+	
+	console.log(width);
+	
+	});	
+	
+	
