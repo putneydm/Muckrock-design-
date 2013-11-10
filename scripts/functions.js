ec8fea134275ed48b7c65434e8f19a4e11aff6f6
putneydm <david@davidputney.com>
2013-11-10 14:39:48 -0500

    Revamped list view and grid view switcher so that it knows the state of the view so it doesn’t try to add the same view as the current state.

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,6 +1,8 @@
 $(document) .ready (function()
 	{	
 
+		
+
 	
 	function mobile_menu (document_width)
 		{
@@ -85,26 +87,71 @@ $(document) .ready (function()
 			});
 		
 		
-		$('#grid') .click (function()
-		{
+		$('.switcher') .click (function()
+		{	
+		var myClass = $('#document_list').attr("class");
+		var button = $(this).attr("title");
+
+		$('.switcher') .removeClass ('clicked');		
+		$(this) .addClass ('clicked');
+	
+		console.log(button);
+	
+		myClass = myClass.replace(/dataTable/ , '');
+		myClass = myClass.replace(/\s/ , '');
+		
+		
+		
+		if (myClass != 'list_view' & button != 'grid')
+			{
+			$('#document_list') .fadeOut (0);
+			$('#document_list') .removeClass ('grid_view') .addClass ('list_view');	
+			$('#document_list') .fadeIn (100);	
+			
+			console.log(myClass);
+			}
+			
+			if (myClass != 'grid_view'  & button != 'list')
+			{
+			$('#document_list') .fadeOut (0);
+		$('#document_list') .removeClass ('list_view') .addClass ('grid_view');	
+		$('#document_list') .fadeIn (100);	
+			
+			console.log(myClass);
+			}	
+			
+			
+			
+			});
+		
+
+
+/*
+		
+		
+		//console.log('grid');
 		$('#document_list') .fadeOut (0);
-		$('#document_list') .removeClass ('fork_document_list_view') .addClass ('fork_document_grid');	
+		$('#document_list') .removeClass ('list_view') .addClass ('grid_view');	
 		$('#document_list') .fadeIn (100);	
 		});
 		
 	
 		$('#list') .click (function()
 		{
+			//	console.log('list');
 		$('#document_list') .fadeOut (0);
-		$('#document_list') .removeClass ('fork_document_grid') .addClass ('fork_document_list_view');;	
+		$('#document_list') .removeClass ('grid_view') .addClass ('list_view');;	
 		$('#document_list') .fadeIn (100);	
 		});
+	*/
 	
 	
+		});	
 	
 	
 	//resizes input text box on review page to show all the text 
 	
+	/*
 			$('#request_detail_review').css('height','auto')
 		    var text = jQuery('#request_detail_review').val(),
             // look for any "\n" occurences
@@ -118,7 +165,7 @@ $(document) .ready (function()
 		
 	});	
 	
-
+*/
 function menu(scroll_position, menu_height)
 		{		
 		if (scroll_position	> 100 && menu_status==0)
@@ -170,6 +217,7 @@ $(window) .resize (function()
 	$('#measure_width') .text (width);
 	
 	console.log(width);
+	
 	
 	});	
 	
